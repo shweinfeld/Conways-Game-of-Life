08c282e73923f7252a72a493a55b5a305ab013d5
shana <shanaw1999@gmail.com>
2020-11-04 10:05:01 -0500

    Play around with Frame

--- src/Conways/Game/Of/Life/ButtonListener.java
+++ src/Conways/Game/Of/Life/ButtonListener.java
@@ -8,24 +8,24 @@ import java.awt.event.MouseEvent;
 
 public class ButtonListener implements ActionListener {
     private final Grid grid;
-    private final GridView view;
+    private final GridFrame frame;
 
 
-    public ButtonListener(Grid grid, GridView view) {
+    public ButtonListener(Grid grid, GridFrame frame) {
         this.grid = grid;
-        this.view = view;
+        this.frame = frame;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        for (int row = 0; row < grid.getHeight(); row++) {
-            for (int col = 0; col < grid.getWidth(); col++) {
-                if (view.getButtonGrid[row][col] ==  e.getSource()) {
-                    if (grid.getCell(row, col) == true) {
-                        grid.setCell(row, col, false);
+        for (int row = 0; row < GridFrame.ROWS; row++) {
+            for (int col = 0; col < GridFrame.COLS; col++) {
+                if (frame.getButtonGrid()[row][col] ==  e.getSource()) {
+                    if (grid.getCells(row, col) == true) {
+                        grid.setCells(row, col, false);
                     }
                     else {
-                        grid.setCell(row, col, true);
+                        grid.setCells(row, col, true);
                     }
                 }
             }
