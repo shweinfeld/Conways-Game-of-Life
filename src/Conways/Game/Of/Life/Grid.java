c265f23d132f3d542f2c9b0a1509317573f8256e
Jennifer Komendant <jennifer.komendant@gmail.com>
2020-11-04 18:08:18 -0500

    created methods for Grid

--- src/Conways/Game/Of/Life/Grid.java
+++ src/Conways/Game/Of/Life/Grid.java
@@ -2,5 +2,29 @@ package Conways.Game.Of.Life;
 
 public class Grid {
 
-    //Chashie Komendant
+    /**
+     *
+     * Instantiates the grid and decides if the cell will be alive
+     * or dead in the next generation
+     *
+     * Author: Chashie Komendant
+     */
+
+    private final Boolean[][] gridArray = new Boolean[30][50];
+
+    public Grid() {
+
+    }
+
+    public void populateGrid(){
+
+    }
+
+    public void goToNextGeneration(){
+
+    }
+
+    public Boolean[][] getGridArray() {
+        return gridArray;
+    }
 }
