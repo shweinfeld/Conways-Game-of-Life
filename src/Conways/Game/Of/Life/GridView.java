1eb6d3702f8fdbb4cfcfcbc75656d5054d461f43
Ricki Peikes <rickipeikes@gmail.com>
2020-11-04 18:58:44 -0500

    added methods to draw gridlines

--- src/Conways/Game/Of/Life/GridView.java
+++ src/Conways/Game/Of/Life/GridView.java
@@ -15,11 +15,14 @@ public class GridView extends JComponent {
     public static final int CELL_SIZE = 10;
     public static final int HEIGHT = 30;
     public static final int WIDTH = 50;
-    public static final Color LIVE_COLOR = Color.WHITE;
-    public static final Color DEAD_COLOR = Color.BLACK;
-    int[][] squares;
+    public static final Color LIVE_COLOR = Color.ORANGE;
+    public static final Color DEAD_COLOR = Color.GRAY;
+    public static final Color LINE_COLOR = Color.BLACK;
+    Grid grid;
+    boolean[][] squares;
 
     public GridView(Grid grid) {
+        this.grid = grid;
     }
 
     @Override
@@ -31,11 +34,13 @@ public class GridView extends JComponent {
     }
 
     private void paintGrid(Graphics g) {
+        //draw vertical grid lines
         for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                g.drawRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-
-            }
+            g.drawLine(x * CELL_SIZE, 0, x * CELL_SIZE, CELL_SIZE * HEIGHT);
+        }
+        //draw horizontal grid lines
+        for (int y = 0; y < HEIGHT; y++) {
+            g.drawLine(0, y * CELL_SIZE, CELL_SIZE * WIDTH, y * CELL_SIZE);
         }
     }
 
