672b6901b39ea3da42cb17946340ee224086c691
Ricki Peikes <rickipeikes@gmail.com>
2020-11-04 21:09:05 -0500

    Grid implementation successful

--- src/Conways/Game/Of/Life/GridView.java
+++ src/Conways/Game/Of/Life/GridView.java
@@ -12,7 +12,8 @@ import java.awt.*;
 public class GridView extends JComponent {
 
     //private final Grid grid;
-    public static final int CELL_SIZE = 10;
+    public static final int CELL_SIZE = 15;
+    public static final int BORDERED_CELL_SIZE = 17;
     public static final int LINE_SIZE = 1;
     public static final int HEIGHT = 30;
     public static final int WIDTH = 50;
@@ -20,7 +21,7 @@ public class GridView extends JComponent {
     public static final Color DEAD_COLOR = Color.GRAY;
     public static final Color LINE_COLOR = Color.BLACK;
     Grid grid;
-    boolean[][] squares;
+    //boolean[][] squares;
 
     public GridView(Grid grid) {
         this.grid = grid;
@@ -30,39 +31,30 @@ public class GridView extends JComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         paintGrid(g);
-        paintLifeStatus(g);
+        //paintLifeStatus(g);
     }
 
-    private void paintLifeStatus(Graphics g) {
-        for (int i = 0; i < WIDTH; i++) {
-            for (int j = 0; j < HEIGHT; j++) {
-                //not sure why ternary wasn't working
-                if (grid.getCells(i, j)) {
-                    g.setColor(LIVE_COLOR);
-                } else {
-                    g.setColor(DEAD_COLOR);
-                }
-            }
-        }
-    }
+//    private void paintLifeStatus(Graphics g) {
+//        for (int i = 0; i < WIDTH; i++) {
+//            for (int j = 0; j < HEIGHT; j++) {
+//                //not sure why ternary wasn't working
+//                if (grid.getCells(i, j)) {
+//                    g.setColor(LIVE_COLOR);
+//                } else {
+//                    g.setColor(DEAD_COLOR);
+//                }
+//            }
+//        }
+//    }
 
     private void paintGrid(Graphics g) {
-
-        //draw vertical grid lines
-        for (int x = 0; x < WIDTH; x++) {
-            g.setColor(LINE_COLOR);
-            g.fillRect(x * CELL_SIZE, 0, LINE_SIZE, CELL_SIZE * HEIGHT);
-            g.setColor(DEAD_COLOR);
-            for (int i = 0; i < HEIGHT; i++) {
-                g.fillRect(x * CELL_SIZE + 1, i * CELL_SIZE + 1, LINE_SIZE, CELL_SIZE * HEIGHT);
+        g.setColor(LINE_COLOR);
+        g.fillRect(0, 0, WIDTH * BORDERED_CELL_SIZE, HEIGHT * BORDERED_CELL_SIZE);
+        g.setColor(DEAD_COLOR);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                g.fillRect(i * BORDERED_CELL_SIZE, j * BORDERED_CELL_SIZE, CELL_SIZE, CELL_SIZE);
             }
         }
-
-        //draw horizontal grid lines
-        for (int y = 0; y < HEIGHT; y++) {
-            g.setColor(LINE_COLOR);
-            g.fillRect(0, y * CELL_SIZE, WIDTH * CELL_SIZE, LINE_SIZE);
-        }
     }
-
 }
