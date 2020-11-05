2dc3c424980cbe0eee1dcd5c0ad294e303f11fa7
Jennifer Komendant <jennifer.komendant@gmail.com>
2020-11-05 01:06:55 -0500

    removed underscore

--- src/Conways/Game/Of/Life/MouseListener.java
+++ src/Conways/Game/Of/Life/MouseListener.java
@@ -2,7 +2,7 @@ package Conways.Game.Of.Life;
 
 import java.awt.event.MouseEvent;
 
-import static Conways.Game.Of.Life.GridView.CELL_SIZE;
+import static Conways.Game.Of.Life.GridView.CELLSIZE;
 
 public class MouseListener implements java.awt.event.MouseListener {
     private final Grid grid;
@@ -17,8 +17,8 @@ public class MouseListener implements java.awt.event.MouseListener {
     public void mouseClicked(MouseEvent e) {
         int row = e.getX();
         int col = e.getY();
-        int rowIndex = (int)(row / CELL_SIZE);
-        int colIndex = (int)(col/ CELL_SIZE);
+        int rowIndex = (int)(row / CELLSIZE);
+        int colIndex = (int)(col/ CELLSIZE);
 
         if (grid.getCells(rowIndex, colIndex)) {
             grid.setCells(rowIndex, colIndex, false);
